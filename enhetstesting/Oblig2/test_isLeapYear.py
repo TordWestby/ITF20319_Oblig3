74c1c2a2f6135302ac306a5e17b0b93db422160e
TordWestby <tordhw@gmail.com>
2023-10-25 02:42:22 +0200

    Revert "La til noen "tester""
    This reverts commit 8a05fc54c78e2833ac0df239f0b5eeb568c6f88d.

--- enhetstesting/Oblig2/test_isLeapYear.py
+++ enhetstesting/Oblig2/test_isLeapYear.py
@@ -2,30 +2,15 @@ from Oblig2.isLeapYear import isLeapYear
 
 def test_leap_year_dividable_by_4_but_not_100():
     assert isLeapYear(2004) == True
-    assert isLeapYear(2008) == True
-    assert isLeapYear(2012) == True
-    assert isLeapYear(2016) == True
-    assert isLeapYear(2020) == True
-    assert isLeapYear(2000) == True
 
 def test_leap_year_dividable_by_400():
-    assert isLeapYear(400) == True
-    assert isLeapYear(800) == True
-    assert isLeapYear(1200) == True
-    assert isLeapYear(1600) == True
     assert isLeapYear(2000) == True
-
 
 def test_non_leap_year_not_dividable_by_4():
     assert isLeapYear(2005) == False
-    assert isLeapYear(2007) == False
-    assert isLeapYear(2009) == False
-    assert isLeapYear(2011) == False
-    assert isLeapYear(2013) == False
 
 def test_non_leap_year_dividable_by_100_but_not_400():
     assert isLeapYear(1700) == False
 
 def test_leap_year_dividable_by_4000():
     assert isLeapYear(8000) == False
-
