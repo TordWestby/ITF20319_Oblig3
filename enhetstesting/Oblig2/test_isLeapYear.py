40ba6d063f750f15de9ed1e5e9e148e979a8ee18
TordWestby <tordhw@gmail.com>
2023-10-25 14:41:45 +0200

    Feil med vilje
    Bruker en feil med vilje for å vise hvordan det ser ut i actions om en test failer

--- enhetstesting/Oblig2/test_isLeapYear.py
+++ enhetstesting/Oblig2/test_isLeapYear.py
@@ -16,9 +16,13 @@ def test_leap_year_dividable_by_400():
 
 def test_non_leap_year_not_dividable_by_4():
     assert isLeapYear(2005) == False
+    assert isLeapYear(2007) == False
+    assert isLeapYear(2009) == False
+    assert isLeapYear(2011) == False
 
 def test_non_leap_year_dividable_by_100_but_not_400():
     assert isLeapYear(1700) == False
+    assert isLeapYear(1200) == False
 
 def test_leap_year_dividable_by_4000():
     assert isLeapYear(8000) == False
