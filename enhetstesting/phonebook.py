0dac967c0d44677ea8258ddb7809e02f7b3bb9c2
TordWestby <102856887+TordWestby@users.noreply.github.com>
2023-10-25 03:12:17 +0200

    prøver å fjerne phonebook
    prøver å fjerne phonebook og test_phonebook
    Var her koden gikk helt feil sist

--- enhetstesting/phonebook.py
+++
@@ -1,9 +0,0 @@
-class Phonebook:
-    def __init__(self) -> None:
-        self.numbers = {}
-
-    def add(self, name, number):
-        self.numbers[name] = number
-
-    def lookup(self, name):
-        return self.numbers.get(name)
